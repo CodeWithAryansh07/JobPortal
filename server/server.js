1e41254556e2ee94c2b28de515780cdc77a1d2b3
CodeWithAryansh07 <codewitharyansh07@gmail.com>
2025-01-02 11:08:31 +0530

    Clerk Update

--- server/server.js
+++ server/server.js
@@ -29,7 +29,7 @@ app.get("/debug-sentry", function mainHandler(req, res) {
     throw new Error("My first Sentry error!");
 });
 
-app.post('webhooks', clerkWebHooks);
+app.post('/webhooks', clerkWebHooks);
 
 // Port 
 
